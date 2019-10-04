056132c744c02847a55d8525411bcb4d53e24562
twinkal3453 <twinkalkrraj@gmail.com>
2019-10-04 15:12:05 +0530

    Update EndScript.cs

--- EndScript.cs
+++ EndScript.cs
@@ -6,26 +6,18 @@ using UnityEngine.UI;
 
 public class EndScript : MonoBehaviour
 {
-   // public Text scoreText;
    
-    //Use this for initialization
-    void Start()
-    {
-       // gameObject.SetActive(false);
-    }
+   
+   
     public void restartgame()
     {
         SceneManager.LoadScene(1);
     }
-
+     //when you press exit button then applicantion will close
     public void exitgame()
     {
         Debug.Log("Exit Buttn Pressed");
         Application.Quit();
     }
-    //public void ToggleEndMenu(int score)
-    //{
-    //    gameObject.SetActive(true);
-    //    scoreText.text = ((int)score).ToString();
-    //}
+   
 }
